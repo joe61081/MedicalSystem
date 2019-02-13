e4631ab09f2791b7ff5e36fe333667cbe8245d2d
joe61081 <joe61081@mastek.com>
2019-02-13 13:43:28 +0000

    Manager Module Create and Read

---
+++ MedicalApplication/example/Manager.py
@@ -0,0 +1,50 @@
+from  flask.app import Flask
+from flask_sqlalchemy import SQLAlchemy
+import jsonpickle
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/medicalreports'
+
+db = SQLAlchemy(app)
+
+class Manager(db.Model):
+    __tablename__ = "alc_Managers"
+    manager_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column('manager_name', db.String(50))
+    
+    def __init__(self, params):
+        self.name = params["name"]
+    
+    def __str__(self):
+        return "Id: "+str(self.manager_id)+" Name: "+self.name
+   
+
+@app.route("/manager-create")
+def create_manager():
+    man = Manager({"name":"Test Manager 3"})
+    
+    db.session.add(man)
+    db.session.commit()
+    
+    for man in Manager.query.all():
+        print("ID: "+str(man.manager_id)+" Name: "+man.name )
+    
+    return str(Manager.query.all())
+
+@app.route("/manager-fetch")
+def fetch_all_managers():
+    managers = Manager.query.all()
+    
+    for man in managers:
+         print("ID: "+str(man.manager_id)+" Name: "+man.name)
+        
+    return jsonpickle.encode(managers)
+
+if __name__ == '__main__':
+    db.create_all() #create the schema using the alchemy context
+    #example_product()
+    #example_payment()
+    app.run(port=7700)
+    pass
+    
