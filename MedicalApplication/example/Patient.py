e269c49f3edbb3d9ed0c2cd46cea6021e92b79dc
lukerowley101 <44974779+lukerowley101@users.noreply.github.com>
2019-02-14 11:33:59 +0000

    changes

--- MedicalApplication/example/Patient.py
+++ MedicalApplication/example/Patient.py
@@ -4,13 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 import jsonpickle
 from flask.templating import render_template
 from _datetime import datetime
-<<<<<<< HEAD
 from flask.globals import request
-=======
 from flask.globals import request, session
 
 
->>>>>>> branch 'master' of https://github.com/joe61081/MedicalSystem.git
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+mysqlconnector://root:root@localhost:3306/medicalreports'
 db = SQLAlchemy(app)
@@ -23,30 +21,30 @@ class Patient(db.Model):
     patient_date_of_birth = db.Column('patient_date_of_birth',db.String(30))
     patient_location = db.Column('patient_location',db.String(30))
     patient_occupation = db.Column('patient_occupation',db.String(30))
-<<<<<<< HEAD
+
     #manager_id = db.Column(db.Integer,db.ForeignKey('alc_managers.manager_id'),nullable=False)
     #report_id =db.Column(db.Integer,db.ForeginKey('alc_report.report_id'),nullable=False)
-=======
+
     #manager_id = db.Column(db.Integer,db.ForeignKey('alc_Managers.manager_id'),nullable=False)
     #reports = db.relationship("Report", backref=db.backref('Patients', lazy=True))
     
     
     
->>>>>>> branch 'master' of https://github.com/joe61081/MedicalSystem.git
+
     def __init__(self,params):
         self.patient_name = params["patient_name"]
         self.patient_date_of_birth=params["patient_date_of_birth"]
         self.patient_location=params["patient_location"]
         self.patient_occupation=params["patient_occupation"]
-<<<<<<< HEAD
+
         pass
-=======
+
        
         
->>>>>>> branch 'master' of https://github.com/joe61081/MedicalSystem.git
+
     def __str__(self):
         return "Patient Id:"+str(self.patient_id)+"Name:"+self.patient_name+"D.O.B:"+self.patient_date_of_birth+"Location:"+self.patient_date_of_birth+"Occupation:"+self.patient_occupation
-<<<<<<< HEAD
+
 class Report(db.Model):
     __tablename__ = "alc_reports"
     report_id = db.Column(db.Integer,primary_key=True)
@@ -60,6 +58,10 @@ class Report(db.Model):
     
     def __str__(self):
         return "Id:"+str(self.report_id)+" condition:"+self.condition+ "date:"+str(self.date) 
+    
+    
+    
+    
 class Manager(db.Model):
     __tablename__ = "alc_Managers"
     manager_id = db.Column(db.Integer, primary_key=True)
@@ -73,14 +75,12 @@ class Manager(db.Model):
         return "Id: "+str(self.manager_id)+" Name: "+self.name    
  
 # Create Methods
-@app.route("/patient/create")
-=======
+
     
 
 
 
 @app.route('/patient/create', methods = ['POST'])
->>>>>>> branch 'master' of https://github.com/joe61081/MedicalSystem.git
 def create_Patient():
    # p = Patient({"patient_name":"dave","patient_date_of_birth":"19/03/1997","patient_location":"Leeds","patient_occupation":"driver"})
     db.session.add(
@@ -93,8 +93,6 @@ def create_Patient():
     patients = Patient.query.all()
     for p in patients: 
         print("Patient Id:"+str(p.patient_id)+"Name:"+p.patient_name+"D.O.B:"+p.patient_date_of_birth+"Location:"+p.patient_date_of_birth+"Occupation:"+p.patient_occupation)
-<<<<<<< HEAD
-=======
     
     return jsonpickle.encode(patients) 
 @app.route("/patient/example")
@@ -107,11 +105,9 @@ def fetch_all_Patient():
         
     return jsonpickle.encode(patients)    
    
->>>>>>> branch 'master' of https://github.com/joe61081/MedicalSystem.git
 
-<<<<<<< HEAD
-@app.route("/api/insert-patient", methods = ['POST'])
-def insert_Patient():
+
+
     db.session.add(
         Patient({
             "patient_name": request.form.get('patient_name'),
@@ -122,8 +118,8 @@ def insert_Patient():
     db.session.commit()
     
 
-=======
-class Manager(db.Model):
+
+
     __tablename__ = "alc_Managers"
     manager_id = db.Column(db.Integer, primary_key=True)
     name = db.Column('manager_name', db.String(50))
@@ -136,7 +132,7 @@ class Manager(db.Model):
     def __str__(self):
         return "Id: "+str(self.manager_id)+" Name: "+self.name
    
->>>>>>> branch 'master' of https://github.com/joe61081/MedicalSystem.git
+
 
 @app.route("/manager-create")
 def create_manager():
@@ -149,9 +145,9 @@ def create_manager():
         print("ID: "+str(man.manager_id)+" Name: "+man.name )
     
     return jsonpickle.encode(Manager.query.all())
-<<<<<<< HEAD
+
   
-=======
+
 
 @app.route("/manager-fetch")
 def fetch_all_managers():
@@ -181,7 +177,7 @@ class Report(db.Model):
     def __str__(self):
         return "Id:"+str(self.report_id)+" condition:"+self.condition+ "date:"+str(self.date)
 
->>>>>>> branch 'master' of https://github.com/joe61081/MedicalSystem.git
+
 @app.route("/report-create")
 def create_report():
     rep = Report({"condition":"cough","date":getTimestamp()}) 
@@ -223,20 +219,13 @@ def fetch_all_reports():
     return jsonpickle.encode(reports)
 
 if __name__ == '__main__':
-<<<<<<< HEAD
+
     db.create_all()
     create_Patient()
     create_manager()
     create_report()
     
-=======
+
     db.create_all()
-
-    #db.create_all()
-    #create_manager()
-    #create_report()
-    #create_Patient()
-
->>>>>>> branch 'master' of https://github.com/joe61081/MedicalSystem.git
     app.run(port=7700)
     pass
